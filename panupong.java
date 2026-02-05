6e41694d89cfe686583c34ca6acfcb2341e0ec96
I'm having a bad day . <pranupong.49@gmail.com>
2026-02-05 09:05:04 +0700

    Add flie panupong

---
+++ panupong.java
@@ -0,0 +1,12 @@
+public class panupong {
+    public static void main(String[] args) {
+       panupong_display.panupong_display();
+      
+    }
+}
+
+class panupong_display{
+    public static void panupong_display(){
+        System.out.println("panupong_display_method");
+    }
+}
