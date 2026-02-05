3af755a1134d1b0417bd3044946920c1f0b96128
I'm having a bad day . <pranupong.49@gmail.com>
2026-02-05 09:06:24 +0700

    Add display form display

--- panupong.java
+++ panupong.java
@@ -1,6 +1,7 @@
 public class panupong {
     public static void main(String[] args) {
        panupong_display.panupong_display();
+         display.display();
       
     }
 }
@@ -8,5 +9,11 @@ public class panupong {
 class panupong_display{
     public static void panupong_display(){
         System.out.println("panupong_display_method");
+    }
+}
+
+class display{
+    public static void display(){
+        System.out.println("display_method");
     }
 }
