e0703b95eccd4335b701444cfa7a9e71ce9d63bc
I'm having a bad day . <pranupong.49@gmail.com>
2026-02-05 09:14:39 +0700

    Add show

--- panupong.java
+++ panupong.java
@@ -2,6 +2,7 @@ public class panupong {
     public static void main(String[] args) {
        panupong_display.panupong_display();
          display.display();
+         show.show();
       
     }
 }
@@ -15,5 +16,11 @@ class panupong_display{
 class display{
     public static void display(){
         System.out.println("display_method");
+    }
+}
+
+class show{
+    public static void show(){
+        System.out.println("hello_world");
     }
 }
